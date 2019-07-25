c622a11902af3751bf2b00ebe40ef384f485d055
AdamKniec <adam.k.kniec@gmail.com>
2019-07-25 22:00:39 +0200

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+//compile scss to css
+
+function style() {
+  //WHere si my scss file ?
+  return (
+    gulp
+      .src("./scss**/*.scss")
+      //pass itthrough the compiler
+      .pipe(sass().on("error", sass.logError))
+      //where to save compiled css ?
+      .pipe(gulp.dest("./css"))
+      // changes goes to all browsers
+      .pipe(browserSync.stream())
+  );
+}
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
