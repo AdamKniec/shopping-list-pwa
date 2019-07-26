e846018bc815f91a7b426faae8b7e7c511fc3b25
AdamKniec <adam.k.kniec@gmail.com>
2019-07-26 14:26:28 +0200

    path change gh page test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("/shopping-list-pwa/sw.js")
     .then(reg => console.log("SW registered", reg))
     .catch(err => console.log("SW not registered", err));
 }
